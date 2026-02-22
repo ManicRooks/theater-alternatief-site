afdff78839bf8a4441d5cc6ac008d8fd12766361
Rik <coomansrik@gmail.com>
2026-02-22 16:59:31 +0100

    github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	  site: 'https://manicrooks.github.io',
+	  base: '/theater-alternatief-site/'
 });
+	
