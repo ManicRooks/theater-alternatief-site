e0fa05e20e45b25590f5e5333c771f19e7bbaece
Rik <coomansrik@gmail.com>
2026-02-22 17:45:44 +0100

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	  site: 'https://manicrooks.github.io',
-	  base: '/theater-alternatief-site/'
+	  site: 'https://manicrooks.github.io'	  
 });
 	
